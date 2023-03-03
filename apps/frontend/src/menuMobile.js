db161af9d24d10670712b618eb427af89dab5288
Diego Vergara Casanova <davc93@gmail.com>
2023-03-03 20:07:00 -0300

    finished

---
+++ apps/frontend/src/menuMobile.js
@@ -0,0 +1,33 @@
+import { navbarMobile } from "./nodes";
+import {gsap} from 'gsap'
+function toggleNavbar() {
+    const menuMobile = navbarMobile.querySelector('.navbar--mobile__menu')
+    menuMobile.classList.toggle('active-menu')
+    menuMobile.style = ""
+    if(menuMobile.classList.contains('active-menu')){
+
+        gsap.from(".navbar--mobile__menu",{
+            x:-400
+        })
+    } else {
+
+        gsap.to(".navbar--mobile__menu",{
+            x:-400
+        })
+    }
+}
+function closeNavbar() {
+    navbarMobile.classList.add('inactive')
+}
+function openNavbar() {
+    navbarMobile.classList.remove('inactive')
+}
+
+
+const toggleButton = navbarMobile.querySelector('.toggle-button')
+toggleButton.addEventListener('click',toggleNavbar)
+
+
+export {
+    toggleNavbar
+}
--- apps/frontend/src/models/index.d.ts
+++ apps/frontend/src/models/index.d.ts
@@ -1,3 +1,80 @@
-export interface Global {
-    
+export interface getPreferenceId {
+  async (order:Order):string
 }
+
+export interface Order {
+  preference: Preference;
+  shipment: Shipment;
+}
+
+export interface Preference {
+  items: Item[];
+  payer?: Payer;
+  back_urls?: BackUrls;
+  auto_return?: "approved" | "all" | undefined;
+  payment_methods?: unknown;
+  notification_url?: string;
+  statement_descriptor?: string;
+  external_reference?: string;
+  expires?: boolean;
+  expiration_date_from?: string | undefined;
+  expiration_date_to?: string | undefined;
+  shipments?: unknown;
+}
+
+export interface BackUrls {
+  success: string;
+  failure: string;
+  pending: string;
+}
+
+export interface Item {
+  id: string;
+  title: string;
+  currency_id: unknown;
+  picture_url: string;
+  description: string;
+  category_id: string;
+  quantity: number;
+  unit_price: number;
+}
+
+export interface Payer {
+  name?: string;
+  surname?: string;
+  email: string;
+  phone?: Phone;
+  identification?: Identification;
+  address?: Address;
+}
+
+export interface Address {
+  street_name: string;
+  street_number: number;
+  zip_code: string;
+}
+
+export interface Identification {
+  type: string;
+  number: string;
+}
+
+export interface Phone {
+  area_code: string;
+  number: string;
+}
+
+export interface PaymentMethods {
+  excluded_payment_methods: ExcludedPayment[];
+  excluded_payment_types: ExcludedPayment[];
+  installments: number;
+}
+
+export interface ExcludedPayment {
+  id: string;
+}
+
+export interface Shipment {
+    cost: number | null
+  }
+  
