e3ec788829e82eb0ecf1d06fbc745500f31af060
Diego Vergara Casanova <davc93@gmail.com>
2023-06-21 20:44:53 -0400

    se dividen modulos

--- apps/checkout-vanilla/src/@types/mercadopago.d.ts
+++ apps/checkout-vanilla/src/@types/mercadopago.d.ts
@@ -1,3 +1,19 @@
 interface Window {
     MercadoPago:any
 }
+interface ImportMetaEnv {
+    [key: string]: any
+    BASE_URL: string
+    MODE: string
+    DEV: boolean
+    PROD: boolean
+    SSR: boolean
+  }
+  
+  interface ImportMeta {
+    url: string
+  
+  
+    readonly env: ImportMetaEnv
+  
+  }
